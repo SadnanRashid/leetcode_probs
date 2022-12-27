380d3ac89988b8c3b0e834cee1702089c0472b0a
SadnanRashid <sadnan.rashid.07@gmail.com>
2022-12-27 21:54:26 +0600

    udaptes

--- Reverse Integer.js
+++ Reverse Integer.js
@@ -4,6 +4,12 @@ var reverse = function (number) {
   const numString = Math.abs(number).toString();
   let flag = "";
 
+  //   Check if more than MAX or less than MIN
+  if (number > MAX) {
+    return 0;
+  } else if (number < MIN) {
+    return 0;
+  }
   //   check if the number is neg or pos
   if (number < 0) {
     flag = flag + "-";
@@ -15,4 +21,4 @@ var reverse = function (number) {
   return flag;
 };
 
-console.log(reverse(-123));
+console.log(reverse(-1323214213412423));
