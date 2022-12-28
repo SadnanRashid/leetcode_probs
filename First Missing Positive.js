cf7b17b523428d1c166cb35f955f8f822826e272
SadnanRashid <sadnan.rashid.07@gmail.com>
2022-12-29 02:03:03 +0600

    s

--- First Missing Positive.js
+++ First Missing Positive.js
@@ -1,6 +1,12 @@
 var firstMissingPositive = function (nums) {
-  //
+  const numSet = new Set(nums);
+  const maxLoop = 1000000;
+  for (let i = 1; i < maxLoop; i++) {
+    if (!numSet.has(i)) {
+      return i;
+    }
+  }
 };
 
-let nums = [1, 2, 0];
+let nums = [7, 8, 9, 11, 12];
 console.log(firstMissingPositive(nums));
