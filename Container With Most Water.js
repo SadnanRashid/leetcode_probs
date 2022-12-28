1a2d5585b263c221f80f997e9a61c5d88d18f914
SadnanRashid <sadnan.rashid.07@gmail.com>
2022-12-28 11:00:30 +0600

    s

--- Container With Most Water.js
+++ Container With Most Water.js
@@ -4,11 +4,17 @@ var maxArea = function (height) {
     i = 0,
     j = 0;
   let maxArea = 0;
+  const len = height.length;
 
-  for (i = 0; i < height.length; i++) {
-    for (j = i + 1; j < height.length; j++) {
-      flag = Math.min(height[i], height[j]);
-      temp = flag * Math.abs(i - j);
+  for (i = 0; i < len; i++) {
+    for (j = i + 1; j < len; j++) {
+      if (height[i] > height[j]) {
+        temp = height[j] * (j - i);
+      } else {
+        temp = height[i] * (j - i);
+      }
+      //   flag = Math.min(height[i], height[j]);
+      //   temp = flag * Math.abs(i - j);
       if (temp > maxArea) {
         maxArea = temp;
       }
@@ -17,4 +23,4 @@ var maxArea = function (height) {
   return maxArea;
 };
 
-console.log(maxArea(height));
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
